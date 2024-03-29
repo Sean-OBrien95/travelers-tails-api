35e81a978e36fdada96763a7f08a60285a71181f
Seán O'Brien <sean95ob@gmail.com>
2024-03-29 16:11:55 +0000

    Add comment list and comment detail as well as edit and delete functions

--- comments/serializers.py
+++ comments/serializers.py
@@ -22,3 +22,11 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that it doesn;t have to be updated everytime
+    """
+    post = serializers.ReadOnlyField(source='post.id')
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from travelers_tails_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment when user is logged in.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you are the creator 
+    of the comment.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
