eab1030fd5384d741ceeda1a2aa21c94522350ce
Seán O'Brien <sean95ob@gmail.com>
2024-05-04 12:40:42 +0000

    Update notifications urls and views

--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import generics, permissions
 from .models import Notification
 from .serializers import NotificationSerializer
+from .permissions import IsNotificationOwnerOrReadOnly
 
-class NotificationViewSet(viewsets.ModelViewSet):
+class NotificationList(generics.ListCreateAPIView):
+    """
+    List all notifications and create a new notification.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class NotificationDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve and delete a specific notification.
+    """
+    permission_classes = [IsNotificationOwnerOrReadOnly]
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
