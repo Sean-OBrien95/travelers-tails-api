4fb2a98d0fdbeb22adbba0b618223811285ef94e
Seán O'Brien <sean95ob@gmail.com>
2024-05-13 13:21:35 +0000

    Add profile image fields to serializers for notifications

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -4,6 +4,8 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
 
     username = serializers.ReadOnlyField(source='sender.username')
+    profile_id = serializers.ReadOnlyField(source='sender.profile.id')
+    profile_image = serializers.ReadOnlyField(source='sender.profile.image.url')
 
     class Meta:
         model = Notification
