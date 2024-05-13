1fb3ad55d07fc5d55c3a8581a00765436700fe8e
Seán O'Brien <sean95ob@gmail.com>
2024-05-13 13:34:28 +0000

    Remove id from serializers

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -4,7 +4,6 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
 
     username = serializers.ReadOnlyField(source='sender.username')
-    profile_id = serializers.ReadOnlyField(source='sender.profile.id')
     profile_image = serializers.ReadOnlyField(source='sender.profile.image.url')
 
     class Meta:
