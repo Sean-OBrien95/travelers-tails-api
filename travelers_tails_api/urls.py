44dcf80822c441f0d08496d358f95101077f20bc
Seán O'Brien <sean95ob@gmail.com>
2024-05-14 13:04:10 +0000

    Add messaging to main urls

--- travelers_tails_api/urls.py
+++ travelers_tails_api/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-    path('', include('notifications.urls'))
+    path('', include('notifications.urls')),
+    path('', include('messaging.urls'))
 ]
