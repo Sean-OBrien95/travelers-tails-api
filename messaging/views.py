4574eaef774e2d9ef3684f061a868829c56f93f1
Seán O'Brien <sean95ob@gmail.com>
2024-05-22 16:09:28 +0000

    Import API views

--- messaging/views.py
+++ messaging/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import ChatMessage
 from .serializers import MessageSerializer
 from travelers_tails_api.serializers import CurrentUserSerializer
+from rest_framework.views import APIView
 
 
 class CustomInbox(generics.ListAPIView):
